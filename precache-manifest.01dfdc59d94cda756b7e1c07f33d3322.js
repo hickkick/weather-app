eaaa12d83644a538979285e864e9790f2197233b
Roman Oliinyk <inthewavves@gmail.com>
2020-01-18 23:12:27 +0200

    Updates

---
+++ precache-manifest.01dfdc59d94cda756b7e1c07f33d3322.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba4038fc2101a65aadbca9aa77f4f520",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "9ad27621ecf587818413",
+    "url": "/weather-app/static/css/main.0bc95370.chunk.css"
+  },
+  {
+    "revision": "b4cb036cebda0d748229",
+    "url": "/weather-app/static/js/2.02c44a9a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/weather-app/static/js/2.02c44a9a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9ad27621ecf587818413",
+    "url": "/weather-app/static/js/main.50d5ee51.chunk.js"
+  },
+  {
+    "revision": "48f3787e04d45de6cbd1",
+    "url": "/weather-app/static/js/runtime-main.36d91d67.js"
+  }
+]);
